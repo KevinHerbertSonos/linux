1565b993dd9e186ffd285390bb565ed0b36fcc17
Kevin Paul Herbert <kevin.herbert@sonos.com>
2022-05-20 18:11:47 -0700

    patch 049_sonos_adv7533_10msec_pd_reset.patch

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -1205,7 +1205,10 @@ static int adv7511_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 	}
 
 	if (adv7511->gpio_pd) {
-		usleep_range(5000, 6000);
+		if (adv7511->type == ADV7511)
+			usleep_range(5000, 6000);
+		else
+			usleep_range(10000, 11000);
 		gpiod_set_value_cansleep(adv7511->gpio_pd, 0);
 	}
 
