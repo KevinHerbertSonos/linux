6d9a857876f5b831e504a88adcd5fee6ef8bbea1
Kevin Paul Herbert <kevin.herbert@sonos.com>
2022-05-20 18:14:43 -0700

    patch 352_mtk_spdif_in_missed_rate_change.patch

--- sound/soc/mediatek/mt8518/mt8518-afe-utils.c
+++ sound/soc/mediatek/mt8518/mt8518-afe-utils.c
@@ -571,6 +571,11 @@ static int mt8518_afe_disable_spdif_in(struct mtk_base_afe *afe)
 	spin_lock_irqsave(&afe_priv->spdifin_ctrl_lock, flags);
 
 	regmap_update_bits(afe->regmap,
+			   AFE_SPDIFIN_INT_EXT2,
+			   AFE_SPDIFIN_INT_EXT2_LRCK_CHANGE_EN_MASK,
+			   0x0);
+
+	regmap_update_bits(afe->regmap,
 			   AFE_SPDIFIN_CFG0,
 			   AFE_SPDIFIN_CFG0_EN,
 			   0x0);
