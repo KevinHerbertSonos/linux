856cdabbf6a1c3535a3987d270b8b8fdff3919e3
Kevin Paul Herbert <kevin.herbert@sonos.com>
2022-07-08 04:37:40 +0000

    patch sonos_mt8518_spdifin_error_irq_mitigate.patch
    From 9e3216060c3ac984eabc16f65b7a292c6752ffff Mon Sep 17 00:00:00 2001
    From: Kerry Calvert <kerry.calvert@sonos.com>
    Date: Tue, 18 Jan 2022 11:20:18 -0800
    Subject: [PATCH] sonos_mt8518_spdifin_error_irq_mitigate

--- sound/soc/mediatek/mt8518/mt8518-afe-common.h
+++ sound/soc/mediatek/mt8518/mt8518-afe-common.h
@@ -539,6 +539,7 @@ struct mt8518_afe_private {
 	u32 dl8_max_main_channels;
 	/* locks */
 	spinlock_t afe_ctrl_lock;
+	struct timer_list spdifin_detect_err_timer;
 	struct regmap *topckgen;
 	struct regmap *scpsys;
 	int block_dpidle_ref_cnt;
