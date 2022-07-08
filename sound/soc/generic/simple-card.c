2dab36275e71de9ad14bc09e646c97d1fc9cc61b
Kevin Paul Herbert <kevin.herbert@sonos.com>
2022-07-08 04:37:10 +0000

    patch sonos_simple_card_provide_clk_id.patch
    From 01d571d38a2250bd6f0883aec6f514c5dd79277f Mon Sep 17 00:00:00 2001
    From: Kerry Calvert <kerry.calvert@sonos.com>
    Date: Wed, 12 Jan 2022 08:23:01 -0800
    Subject: [PATCH] sonos_simple_card_provide_clk_id

--- sound/soc/generic/simple-card.c
+++ sound/soc/generic/simple-card.c
@@ -159,12 +159,12 @@ static int asoc_simple_card_hw_params(struct snd_pcm_substream *substream,
 
 	if (mclk_fs) {
 		mclk = params_rate(params) * mclk_fs;
-		ret = snd_soc_dai_set_sysclk(codec_dai, 0, mclk,
+		ret = snd_soc_dai_set_sysclk(codec_dai, dai_props->codec_dai.clk_id, mclk,
 					     SND_SOC_CLOCK_IN);
 		if (ret && ret != -ENOTSUPP)
 			goto err;
 
-		ret = snd_soc_dai_set_sysclk(cpu_dai, 0, mclk,
+		ret = snd_soc_dai_set_sysclk(cpu_dai, dai_props->cpu_dai.clk_id, mclk,
 					     SND_SOC_CLOCK_OUT);
 		if (ret && ret != -ENOTSUPP)
 			goto err;
