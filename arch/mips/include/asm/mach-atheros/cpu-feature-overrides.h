c627ede7de14d7e443eac53757468ee5f00cd3be
Kevin Paul Herbert <kevin.herbert@sonos.com>
2022-07-08 04:36:18 +0000

    to_add /home/kph/Development/pdsw-sonos-controller-player-s2/platforms/linux/patches/2.6.35/

---
+++ arch/mips/include/asm/mach-atheros/cpu-feature-overrides.h
@@ -0,0 +1,9 @@
+#ifndef __ASM_MACH_MIPS_CPU_FEATURE_OVERRIDES_H
+#define __ASM_MACH_MIPS_CPU_FEATURE_OVERRIDES_H
+                                                                                
+#include <generated/autoconf.h>
+
+#define cpu_has_llsc        1
+#define cpu_has_prefetch    1
+
+#endif
