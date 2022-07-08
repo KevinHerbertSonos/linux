4959e63a2cc10e5219675e01a11b6ab065cba130
Kevin Paul Herbert <kevin.herbert@sonos.com>
2022-07-08 04:38:38 +0000

    patch 904_sonos_sec_init_changes.patch
    diff -Naurp --no-dereference old/init/Makefile linux-4.9.99/init/Makefile

--- drivers/tty/sysrq.c
+++ drivers/tty/sysrq.c
@@ -51,6 +51,12 @@
 #include <asm/ptrace.h>
 #include <asm/irq_regs.h>
 
+#ifdef CONFIG_SONOS_SECBOOT
+#include "mdp.h"
+#include "sonos_lock.h"
+extern int is_sysrq_authorized(void);
+#endif
+
 /* Whether we react on sysrq keys or just ignore them */
 static int __read_mostly sysrq_enabled = CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE;
 static bool __read_mostly sysrq_always_enabled;
@@ -555,6 +561,14 @@ void __handle_sysrq(int key, bool check_mask)
 	int orig_log_level;
 	int i;
 
+#ifdef CONFIG_SONOS_SECBOOT
+#ifndef CONFIG_OPROFILE
+	if ( !is_sysrq_authorized() ) {
+		printk("sysrq handling is disabled\n");
+		return ;
+	}
+#endif
+#endif
 	rcu_sysrq_start();
 	rcu_read_lock();
 	/*
