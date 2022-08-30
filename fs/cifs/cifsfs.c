80de3ec88f6f4575f6412a8e3e0428e016c99229
Mathew Prokos <mathew.prokos@sonos.com>
2022-08-30 07:17:08 -0700

    sonos cifs smb tweaks
    Enables cifs NTLMSSP support on Sonos products.

--- fs/cifs/cifsfs.c
+++ fs/cifs/cifsfs.c
@@ -67,7 +67,7 @@
 
 int cifsFYI = 0;
 bool traceSMB;
-bool enable_oplocks = true;
+bool enable_oplocks = false;	/* Sonos */
 bool linuxExtEnabled = true;
 bool lookupCacheEnabled = true;
 bool disable_legacy_dialects; /* false by default */
