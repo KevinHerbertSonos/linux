9da88a41543a222582aef683f3ce5ae8321f0da2
Milena Milenkovic <milena.milenkovic@sonos.com>
2022-09-12 15:12:10 -0500

    sonos_sec_format_security

--- drivers/base/power/sysfs.c
+++ drivers/base/power/sysfs.c
@@ -171,7 +171,7 @@ static ssize_t runtime_status_show(struct device *dev,
 			return -EIO;
 		}
 	}
-	return sprintf(buf, p);
+	return sprintf(buf, "%s", p); /* Sonos: format_security */
 }
 
 static DEVICE_ATTR_RO(runtime_status);
