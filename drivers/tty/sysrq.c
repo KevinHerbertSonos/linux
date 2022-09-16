97cc233e2f2500b6eb69f087139085293f982e75
Milena Milenkovic <milena.milenkovic@sonos.com>
2022-09-15 20:05:33 -0500

    sonos_sec_init_changes

--- drivers/tty/sysrq.c
+++ drivers/tty/sysrq.c
@@ -55,6 +55,12 @@
 #include <asm/ptrace.h>
 #include <asm/irq_regs.h>
 
+#ifdef CONFIG_SONOS_SECBOOT
+#include "mdp.h"
+#include "sonos_lock.h"
+extern int is_sysrq_authorized(void);
+#endif
+
 /* Whether we react on sysrq keys or just ignore them */
 static int __read_mostly sysrq_enabled = CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE;
 static bool __read_mostly sysrq_always_enabled;
@@ -590,6 +596,14 @@ void __handle_sysrq(u8 key, bool check_mask)
 	orig_suppress_printk = suppress_printk;
 	suppress_printk = 0;
 
+#ifdef CONFIG_SONOS_SECBOOT
+#ifndef CONFIG_OPROFILE
+	if (!is_sysrq_authorized()) {
+		printk("sysrq handling is disabled\n");
+		return;
+	}
+#endif
+#endif
 	rcu_sysrq_start();
 	rcu_read_lock();
 	/*
