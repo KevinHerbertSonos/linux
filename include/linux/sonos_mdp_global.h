20402ccf5d09652b384d9470c51391a8141e1a0d
Milena Milenkovic <milena.milenkovic@sonos.com>
2022-09-15 22:12:29 -0500

    sonos_sec_lock_general

---
+++ include/linux/sonos_mdp_global.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2018, Sonos, Inc.
+ *
+ * SPDX-License-Identifier:	GPL-2.0
+ */
+
+#ifndef SONOS_MDP_GLOBAL_H
+#define SONOS_MDP_GLOBAL_H
+
+#include "mdp.h"
+
+extern struct manufacturing_data_page sys_mdp;
+extern struct manufacturing_data_page3 sys_mdp3;
+
+#endif
