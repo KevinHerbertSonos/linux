e702b5ae88c18a45bffc1e954b36e0356097ad38
Ed Okerson <ed.okerson@sonos.com>
2023-07-25 19:38:53 -0500

    This changelist allows any CEC client with a configured adapter transmit using the unregistered Logical Address (LA=15). This address is cannot receive unicast messages as there is no reciprocal unregistered receive address. It cannot be polled but it can receive broadcast messages. The address also has no formal allocation process since it is, as the name implies, unregistered. Any number of devices on the CEC bus can simultaneously hold the unregistered address.
    Normally Linux performs a validity check to make sure the message queued for
    transmit originates from the allocated LA.  Messages failing this check are not
    queued and are errored out.  To officially operate under this restriction would
    require a userspace client to allocate more logical addresses (4 can be allocated
    total).  However allocating a secondary unregistered logical address to pass
    a single vailidity check provides no additional benefit over the primary logical
    address.
    
    This patch alters the validity check to allow all configured adapters to
    transmit on their allocated address and the unregistered address without
    needing to formally allocate LA=15.

--- drivers/media/cec/cec-adap.c
+++ drivers/media/cec/cec-adap.c
@@ -837,6 +837,7 @@ int cec_transmit_msg_fh(struct cec_adapter *adap, struct cec_msg *msg,
 			return -EINVAL;
 		}
 		if (msg->len > 1 && adap->is_configured &&
+			(cec_msg_initiator(msg) != CEC_LOG_ADDR_UNREGISTERED) &&
 		    !cec_has_log_addr(adap, cec_msg_initiator(msg))) {
 			dprintk(1, "%s: initiator has unknown logical address %d\n",
 				__func__, cec_msg_initiator(msg));
