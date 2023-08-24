e8270bc143f922441246f46943783e4341598b17
Milena Milenkovic <milena.milenkovic@sonos.com>
2023-08-23 22:54:24 -0500

    Add keys for jaws and pallas arch to Linux 5.4

--- crypto/asymmetric_keys/sonos_signature_keys_s767.c
+++ crypto/asymmetric_keys/sonos_signature_keys_s767.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2022, Sonos, Inc.
+ * Copyright (c) 2015-2023, Sonos, Inc.
  *
  * SPDX-License-Identifier:	GPL-2.0
  *
@@ -18,11 +18,17 @@
 #include <crypto/sonos_signature_keys.h>
 #include <linux/sonos_kernel.h>
 
+extern SonosRsaKeyEntry SRKE_unlock_jaws;
+extern SonosRsaKeyEntry SRKE_unitCA_jaws;
+
 extern SonosRsaKeyEntry SRKE_unlock_lasso;
 extern SonosRsaKeyEntry SRKE_unitCA_lasso;
 
 extern SonosRsaKeyEntry SRKE_unlock_optimo;
 extern SonosRsaKeyEntry SRKE_unitCA_optimo;
+
+extern SonosRsaKeyEntry SRKE_unlock_pallas;
+extern SonosRsaKeyEntry SRKE_unitCA_pallas;
 
 extern SonosRsaKeyEntry SRKE_unlock_prima;
 extern SonosRsaKeyEntry SRKE_unitCA_prima;
@@ -35,14 +41,32 @@ sonosInitKeyTable(void)
 	// run-time but preserve the constness of this array to external callers.
 	if (sonos_product_id == PRODUCT_ID_OPTIMO1 ||
 	    sonos_product_id == PRODUCT_ID_OPTIMO1SL || 
-	    sonos_product_id == PRODUCT_ID_OPTIMO2) {
+	    sonos_product_id == PRODUCT_ID_OPTIMO2 ||
+            sonos_product_id == PRODUCT_ID_RAVEN) {
 		g_SonosSigningKeys[0] = &SRKE_unlock_optimo;
 		g_SonosSigningKeys[1] = &SRKE_unitCA_optimo;
 	}
+// TODO FIXME remove this if 0 in the arch feature branch,
+// when the arch is defined without STUB_SECBOOT_ARCH_KEYS
+#if 0
+	else if (sonos_product_id == PRODUCT_ID_JAWS ||
+                 sonos_product_id == PRODUCT_ID_LOTUS) {
+		g_SonosSigningKeys[0] = &SRKE_unlock_jaws;
+		g_SonosSigningKeys[1] = &SRKE_unitCA_jaws;
+	}
+#endif
 	else if (sonos_product_id == PRODUCT_ID_LASSO) {
 		g_SonosSigningKeys[0] = &SRKE_unlock_lasso;
 		g_SonosSigningKeys[1] = &SRKE_unitCA_lasso;
 	}
+// TODO FIXME remove this if 0 in the arch feature branch,
+// when the arch is defined without STUB_SECBOOT_ARCH_KEYS
+#if 0
+	else if (sonos_product_id == PRODUCT_ID_PALLAS_PLUS) {
+		g_SonosSigningKeys[0] = &SRKE_unlock_pallas;
+		g_SonosSigningKeys[1] = &SRKE_unitCA_pallas;
+	}
+#endif
 	else if (sonos_product_id == PRODUCT_ID_PRIMA) {
 		g_SonosSigningKeys[0] = &SRKE_unlock_prima;
 		g_SonosSigningKeys[1] = &SRKE_unitCA_prima;
