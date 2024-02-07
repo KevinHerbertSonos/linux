2b47bc7c3e98fe2fa94fd1e7f307d8fd65604eb7
lbeverage <lyford.beverage@sonos.com>
2024-02-07 09:08:03 -0500

    SWPBL-222507 - Optimo 5.4 kernel fails to handle USBC ethernet connection change
    This was identified and fixed on 4.9.295 (SWPBL-185980) and that port is needed here, too.

--- drivers/net/usb/r8152.c
+++ drivers/net/usb/r8152.c
@@ -4208,9 +4208,6 @@ static void rtl_hw_phy_work_func_t(struct work_struct *work)
 	mutex_unlock(&tp->control);
 
 	usb_autopm_put_interface(tp->intf);
-#ifdef CONFIG_SONOS
-	sonos_announce_linkup(tp->netdev);
-#endif
 }
 
 #ifdef CONFIG_PM_SLEEP
@@ -5726,6 +5723,10 @@ static int rtl8152_probe(struct usb_interface *intf,
 		device_set_wakeup_enable(&udev->dev, false);
 
 	netif_info(tp, probe, netdev, "%s\n", DRIVER_VERSION);
+
+#ifdef CONFIG_SONOS
+	sonos_announce_linkup(tp->netdev);
+#endif
 
 	return 0;
 
