c76b2e2ffd06b2f4cbe78fbbb2e5f9a2ae84f316
Jose Ignacio Tornos Martinez <jtornosm@redhat.com>
2024-04-17 10:55:13 +0200

    net: usb: ax88179_178a: avoid writing the mac address before first reading
    After the commit d2689b6a86b9 ("net: usb: ax88179_178a: avoid two
    consecutive device resets"), reset operation, in which the default mac
    address from the device is read, is not executed from bind operation and
    the random address, that is pregenerated just in case, is direclty written
    the first time in the device, so the default one from the device is not
    even read. This writing is not dangerous because is volatile and the
    default mac address is not missed.
    
    In order to avoid this and keep the simplification to have only one
    reset and reduce the delays, restore the reset from bind operation and
    remove the reset that is commanded from open operation. The behavior is
    the same but everything is ready for usbnet_probe.
    
    Tested with ASIX AX88179 USB Gigabit Ethernet devices.
    Restore the old behavior for the rest of possible devices because I don't
    have the hardware to test.
    
    cc: stable@vger.kernel.org # 6.6+
    Fixes: d2689b6a86b9 ("net: usb: ax88179_178a: avoid two consecutive device resets")
    Reported-by: Jarkko Palviainen <jarkko.palviainen@gmail.com>
    Signed-off-by: Jose Ignacio Tornos Martinez <jtornosm@redhat.com>
    Link: https://lore.kernel.org/r/20240417085524.219532-1-jtornosm@redhat.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/usb/ax88179_178a.c
+++ drivers/net/usb/ax88179_178a.c
@@ -1235,6 +1235,8 @@ static int ax88179_bind(struct usbnet *dev, struct usb_interface *intf)
 	dev->net->hw_features |= NETIF_F_IP_CSUM | NETIF_F_IPV6_CSUM |
 				 NETIF_F_RXCSUM;
 
+	ax88179_reset(dev);
+
 	return 0;
 }
 
@@ -1609,7 +1611,6 @@ static const struct driver_info ax88179_info = {
 	.unbind = ax88179_unbind,
 	.status = ax88179_status,
 	.link_reset = ax88179_link_reset,
-	.reset = ax88179_reset,
 	.stop = ax88179_stop,
 	.flags = FLAG_ETHER | FLAG_FRAMING_AX,
 	.rx_fixup = ax88179_rx_fixup,
@@ -1622,7 +1623,6 @@ static const struct driver_info ax88178a_info = {
 	.unbind = ax88179_unbind,
 	.status = ax88179_status,
 	.link_reset = ax88179_link_reset,
-	.reset = ax88179_reset,
 	.stop = ax88179_stop,
 	.flags = FLAG_ETHER | FLAG_FRAMING_AX,
 	.rx_fixup = ax88179_rx_fixup,
