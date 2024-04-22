41d0ab603d8f382ce454b0d3a899d61845d36595
jeff chen <jeff.chen@sonos.com>
2024-04-22 10:43:53 +0800

    HWDIAG-2204 - Add eMMC erase/trim cmd enabling patch

--- drivers/mmc/host/meson-gx-mmc.c
+++ drivers/mmc/host/meson-gx-mmc.c
@@ -1227,6 +1227,10 @@ static int meson_mmc_probe(struct platform_device *pdev)
 
 	spin_lock_init(&host->lock);
 
+	if (aml_card_type_mmc(host))
+		mmc->caps |= MMC_CAP_ERASE | MMC_CAP_CMD23;
+		//mmc->caps |= MMC_CAP_CMD23;
+
 	if (host->dram_access_quirk) {
 		/* Limit segments to 1 due to low available sram memory */
 		mmc->max_segs = 1;
