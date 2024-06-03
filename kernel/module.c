d717c6ca27cbc931c391c9ed8d5f60987683c48b
Ed Okerson <ed.okerson@sonos.com>
2024-06-03 17:37:26 -0500

    Revert "disable license check" 5-4-210
    This reverts commit 264d7801072680c279fa0a2d01b74c2d22d69b34.

--- kernel/module.c
+++ kernel/module.c
@@ -1428,9 +1428,6 @@ static int verify_namespace_is_imported(const struct load_info *info,
 
 static bool inherit_taint(struct module *mod, struct module *owner)
 {
-#ifdef CONFIG_SONOS_DISABLE_LICENSE_CHECK
-	return true;
-#endif
 	if (!owner || !test_bit(TAINT_PROPRIETARY_MODULE, &owner->taints))
 		return true;
 
@@ -3333,10 +3330,6 @@ static int move_module(struct module *mod, struct load_info *info)
 
 static int check_module_license_and_versions(struct module *mod)
 {
-#ifdef CONFIG_SONOS_DISABLE_LICENSE_CHECK
-	pr_err("%s: license checking disabled\n", __func__);
-	return 0;
-#else
 	int prev_taint = test_taint(TAINT_PROPRIETARY_MODULE);
 	/*
 	 * ndiswrapper is under GPL by itself, but loads proprietary modules.
@@ -3373,7 +3366,6 @@ static int check_module_license_and_versions(struct module *mod)
 	}
 #endif
 	return 0;
-#endif
 }
 
 static void flush_module_icache(const struct module *mod)
