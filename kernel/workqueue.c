0c7c1bab4c42cfd00d5f5fd61d1871712add98a9
Nikolay Starkov <nikolay.starkov@sonos.com>
2025-04-23 16:15:59 +0200

    Fix kernel/workqueue.c

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1641,6 +1641,7 @@ static void __queue_delayed_work(int cpu, struct workqueue_struct *wq,
 	WARN_ON_ONCE(!wq);
 #ifndef CONFIG_AMLOGIC_CFI_CLANG
 	WARN_ON_ONCE(timer->function != delayed_work_timer_fn);
+#endif
 	WARN_ON_ONCE(timer_pending(timer));
 	WARN_ON_ONCE(!list_empty(&work->entry));
 
